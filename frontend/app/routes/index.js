9051b9c724b16a74c06a3a4127b3b644ef693756
csparkresearch <csparkresearch@gmail.com>
2017-09-03 13:13:02 +0530

    handle 403 errors

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -7,6 +7,13 @@ export default Route.extend({
     return getJSON('/getPublicScripts');
   },
   actions: {
+    error(reason) {
+      if (reason.status === 404 || reason.status === 403) {
+        return { 'status': false, 'data': {} };
+      } else {
+        return { 'status': false, 'data': {} };
+      }
+    },
     refresh() {
       this.refresh();
     }
